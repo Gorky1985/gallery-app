a26a6fa39bf3eb02bc749830297c10410e210349
Goran Cosic <gorky1985@gmail.com>
2023-11-07 11:38:03 +0100

    first commit

---
+++ src/handlers/auth.js
@@ -0,0 +1,29 @@
+import { signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth";
+import { auth } from "../lib/firebase.config";
+
+const provider = new GoogleAuthProvider();
+
+const FirebaseAuth = {
+  signIn: () => {
+    return new Promise((resolve) => {
+      signInWithPopup(auth, provider)
+        .then((response) => {
+          resolve(response.user);
+        })
+        .catch(console.error);
+    });
+  },
+  signOut: () => {
+    return new Promise((resolve) => {
+      signOut(auth).then(() => console.log("Signed out"));
+      resolve().catch(console.error);
+    });
+  },
+  getCurrentUser: () => {
+    return new Promise((resolve) => {
+      return auth.onAuthStateChanged(resolve);
+    });
+  },
+};
+
+export default FirebaseAuth;
