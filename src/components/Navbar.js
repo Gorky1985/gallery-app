218db8dcbeb60cc4f886b6bb9891fb4457095145
Goran Cosic <gorky1985@gmail.com>
2023-11-07 12:12:03 +0100

    readme edited, screenshot added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -155,7 +155,7 @@ function Navbar() {
     <nav className="navbar navbar-expand-lg navbar-light bg-light mb-5">
       <div className="container-fluid">
         <Link className="navbar-brand" href="#">
-          ⚡ Firestock
+          ⚡Gallery App
         </Link>
         <button
           className="navbar-toggler"
